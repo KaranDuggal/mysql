735860e2f142fc3ff7229b69e41ec1882785ccdc
keerti <keerti@gsbitlabs.com.au Logout>
2021-02-02 16:31:06 +0530

    forgot password token

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,9 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
     },
+    forgotpasswordtoken: {
+      type: Sequelize.STRING,
+    },
     role: {
       type: Sequelize.ENUM('admin', 'seller', 'buyer'),
       defaultValue: 'buyer',
