8fc2d87a94423537376f059cae73cd9dcc2d5337
keerti <keerti@gsbitlabs.com.au Logout>
2021-02-02 12:49:06 +0530

    first time

--- models/index.js
+++ models/index.js
@@ -1,34 +1,34 @@
-const dbconfig = require('../configurations/db.config')
-const Sequelize = require("sequelize");
+// const dbconfig = require('../configurations/db.config')
+// const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize(dbconfig.db.name, dbconfig.db.user, dbconfig.db.password, {
-//     // host: dbConfig.HOST,
-//     dialect: dbconfig.db.dialect,
-//     // operatorsAliases: false,
+// // const sequelize = new Sequelize(dbconfig.db.name, dbconfig.db.user, dbconfig.db.password, {
+// //     // host: dbConfig.HOST,
+// //     dialect: dbconfig.db.dialect,
+// //     // operatorsAliases: false,
+// //     pool: {
+// //       max: dbconfig.pool.max,
+// //       min: dbconfig.pool.min,
+// //       acquire: dbconfig.pool.acquire,
+// //       idle: dbconfig.pool.idle
+// //     }
+// //   });
+// // ==================================================================
+// const sequelize = new Sequelize('cartdb', 'root', '12345', {
+//     dialect: 'mysql',
 //     pool: {
-//       max: dbconfig.pool.max,
-//       min: dbconfig.pool.min,
-//       acquire: dbconfig.pool.acquire,
-//       idle: dbconfig.pool.idle
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
 //     }
 //   });
-// ==================================================================
-const sequelize = new Sequelize('cartdb', 'root', '12345', {
-    dialect: 'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  });
 
 
-const db = {};
+// const db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+// // db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 
-module.exports = db;
+// module.exports = db;
